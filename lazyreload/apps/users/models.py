e7fcf207415ae5aa3576d812b79e561dbdca7344
RubinaBallach <rubina.ballach@gmail.com>
2024-02-14 14:48:31 +0100

    FileExtensionValidator added and 'now' problem solved

--- lazyreload/apps/users/models.py
+++ lazyreload/apps/users/models.py
@@ -4,40 +4,27 @@ from django.utils.timezone import now
 import uuid
 
 
-# Create your models here.
-
 class LazyUserManager(UserManager):
     def create_user(self, email, username, password=None, **extra_fields):
         if not email:
-            raise ValueError('The Email field must be set')
+            raise ValueError("The Email field must be set")
         email = self.normalize_email(email)
-        user = self.model(
-            email=email,
-            username=username,
-            **extra_fields
-        )
+        user = self.model(email=email, username=username, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
 
     def create_superuser(self, email, username, password=None, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
 
-        return self.create_user(
-            email,
-            username,
-            password,
-            **extra_fields
-        )
+        return self.create_user(email, username, password, **extra_fields)
+
 
 class LazyUser(AbstractUser):
     user_id = models.UUIDField(
-        db_index=True,
-        unique=True,
-        default=uuid.uuid4,
-        editable=False
-        )  # index to increase speed
+        db_index=True, unique=True, default=uuid.uuid4, editable=False
+    )  # index to increase speed
     objects = LazyUserManager()
     email = models.EmailField(verbose_name="email", max_length=60, unique=True)
     username = models.CharField(max_length=60, unique=True)
@@ -53,25 +40,36 @@ class LazyUser(AbstractUser):
 
     def __str__(self):
         return self.username
-    
+
     @property
     def name(self):
         return f"{self.first_name} {self.last_name}"
-  
+
 
 class LazyUserProfile(models.Model):
     lazy_user_id = models.AutoField(primary_key=True)
     user = models.OneToOneField(
-       LazyUser,
-       on_delete=models.CASCADE,
-       related_name="profile")
+        LazyUser, on_delete=models.CASCADE, related_name="profile"
+    )
     USE_CASE_CHOICES = {
         "job": "Job Applications",
         "flat": "Flat Applications",
-        "both": "Job&Flat Applications"
+        "both": "Job&Flat Applications",
     }
     use_case = models.CharField(max_length=60, choices=USE_CASE_CHOICES, default="job")
     cv_file = models.FileField(upload_to="cvs", blank=True, null=True)
     cv_text = models.TextField(blank=True, null=True)
-    email = models.EmailField(max_length=60, blank=True)  # user might want to use a different mailadress for applications that for lazyapp signup
-    availability = models.DateField(default=now(), verbose_name="Earliest Start Date")
+    email = models.EmailField(
+        max_length=60, blank=True
+    )  # user might want to use a different mailadress for applications that for lazyapp signup
+    availability = models.DateField(verbose_name="Earliest Start Date")
+
+    def save(self, *args, **kwargs):
+        if not self.email:
+            self.email = self.user.email
+        if not self.availability:
+            self.availability = now()
+        super(LazyUserProfile, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.user.username
--- lazyreload/apps/users/serializers.py
+++ lazyreload/apps/users/serializers.py
@@ -1,15 +1,18 @@
 from rest_framework import serializers
 from .models import LazyUser, LazyUserProfile
 from rest_framework.validators import UniqueValidator
-from django.core.validators import EmailValidator, RegexValidator
+from django.core.validators import (
+    EmailValidator,
+    RegexValidator,
+    FileExtensionValidator,
+)
+from django.utils.timezone import now
 
 
 class CapitalizeNameField(serializers.CharField):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-
     def __call__(self, value):
         return value.title()
+
 
 # class LoginSerializer(TokenObtainPairSerializer):
 #     def validate(self, attrs: Dict[str. Any]) -> Dict[str, str]:
@@ -21,63 +24,83 @@ class CapitalizeNameField(serializers.CharField):
 
 
 class LazyUserSerializer(serializers.ModelSerializer):
-    id = serializers.UUIDField(source='user_id', read_only=True)
+    id = serializers.UUIDField(source="user_id", read_only=True)
     username = serializers.SlugField(
         max_length=60,
         validators=[
-            UniqueValidator(queryset=LazyUser.objects.all(), message="Username already exists."),
-            RegexValidator(r'^\w+$', message='Username cannot contain profanities'),
-            ]
+            UniqueValidator(
+                queryset=LazyUser.objects.all(), message="Username already exists."
+            ),
+            RegexValidator(r"^\w+$", message="Username cannot contain profanities"),
+        ],
     )
     email = serializers.EmailField(
         required=True,
-        validators=[EmailValidator(message="Invalid email address"),]
-        )
-    password = serializers.CharField(
-        write_only=True,
-        style={'input_type': 'password'}
-        )
-    
+        validators=[
+            EmailValidator(message="Invalid email address"),
+        ],
+    )
+    password = serializers.CharField(write_only=True, style={"input_type": "password"})
 
-    
+    def to_representation(self, instance):
+        instance.first_name = CapitalizeNameField()(instance.first_name)
+        instance.last_name = CapitalizeNameField()(instance.last_name)
+        return super().to_representation(instance)
+
+    def create(self, validated_data):
+        user = LazyUser(
+            email=validated_data["email"],
+            username=validated_data["username"],
+            first_name=validated_data["first_name"],
+            last_name=validated_data["last_name"],
+        )
+        user.set_password(validated_data["password"])
+        user.save()
+        return user
+
+    def update(self, validated_data):
+        user = LazyUser(
+            email=validated_data["email"],
+            username=validated_data["username"],
+            first_name=validated_data["first_name"],
+            last_name=validated_data["last_name"],
+        )
+        user.set_password(validated_data["password"])
+        user.save()
+        return user
 
     class Meta:
         model = LazyUser
         fields = [
-            'id',
-            'email',
-            'username',
-            'password',
-            'first_name',
-            'last_name',
+            "id",
+            "email",
+            "username",
+            "password",
+            "first_name",
+            "last_name",
         ]
-        
-    def to_internal_value(self, data):
-        data['first_name'] = CapitalizeNameField(data['first_name'])
-        data['last_name'] = CapitalizeNameField(data['last_name'])
-        return super().to_internal_value(data)
-        
-    def create(self, validated_data):
-        user = LazyUser(
-            email=validated_data['email'],
-            username=validated_data['username'],
-            first_name=validated_data['first_name'],
-            last_name=validated_data['last_name']
-        )
-        user.set_password(validated_data['password'])
-        user.save()
-        return user
 
 
 class LazyUserProfileSerializer(serializers.ModelSerializer):
-    cv_file = serializers.FileField()
+    use_case = serializers.CharField(max_length=60, required=True)
+    if use_case == "job" or "both":
+        cv_file = serializers.FileField(
+            required=True,
+            validators=[
+                FileExtensionValidator(allowed_extensions=["pdf", "doc", "docx"])
+            ],
+        )
+    else:
+        cv_file = serializers.FileField(required=False)
+
+    email = serializers.EmailField(
+        default=LazyUser.email,
+        validators=[
+            EmailValidator(message="Invalid email address"),
+        ],
+    )
+    availability = serializers.DateTimeField(default=now)
 
     class Meta:
         model = LazyUserProfile
-        fields = [
-            'id',
-            'cv_file',
-            'user_id'
-        ]
-    
-
+        fields = ["use_case", "cv_file", "email"]
