bfd749ead8400875adda9a206b0ad61a1b571804
KlaudjaKaraj <38klaudja.karaj@gmail.com>
2024-02-20 15:09:08 +0100

    update and delete user done

--- lazyreload/apps/users/views.py
+++ lazyreload/apps/users/views.py
@@ -10,6 +10,7 @@ from rest_framework import status
 from rest_framework.authentication import TokenAuthentication
 from django.shortcuts import get_object_or_404
 from django.contrib.auth import authenticate, login
+from drf_yasg.utils import swagger_auto_schema
 from .models import LazyUser, LazyUserProfile
 from .serializers import LazyUserSerializer, LazyLoginSerializer, LazyUpdateUserSerializer , LazyUserProfileSerializer  # UpdateUserSerializer
 
@@ -20,29 +21,29 @@ class CreateUserAPI(CreateAPIView):
     queryset = LazyUser.objects.all()
     serializer_class = LazyUserSerializer
 
+    #Creates a new user and generates an authentication token.
     def perform_create(self, serializer):
         user = serializer.save()
         Token.objects.create(user=user)
 
 class LoginView(APIView):
     permission_classes = [AllowAny]
-
     def get(self, request, *args, **kwargs):
         return render(request, 'users/login.html')  
 
+    # Authenticates a user based on provided credentials and returns a token and user information.
     def post(self, request, *args, **kwargs):
         serializer = LazyLoginSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-
-        username = serializer.validated_data.get('username')
-        #email = serializer.validated_data.get('email')  
+        username = serializer.validated_data.get('username') 
         password = serializer.validated_data.get('password')
         user = None
+        
+        #if validation is true, extract username and password from validated data
         if username:
             user = authenticate(request, username=username, password=password)
-        # elif email:
-        #     user = authenticate(request, email=email, password=password)
 
+        #if the user object is valid, the user is logged and the token is created or retrieved
         if user:
             login(request, user)  # Log the user in
             token, created = Token.objects.get_or_create(user=user)
@@ -59,19 +60,43 @@ class LoginView(APIView):
 class LazyUpdateUserAPIView(APIView):
     permission_classes = [IsAuthenticated]
 
+    # Updates a user's information
     def put(self, request, user_id):
         try:
-            user = LazyUser.objects.get(id=user_id)
+            user = LazyUser.objects.get(user_id=user_id)
+            # Check authentication and permissions
+            if not request.user.is_staff and request.user != user:
+                return Response({'detail': 'You are not authorized to update this user.'}, status=status.HTTP_403_FORBIDDEN)
+
+            serializer = LazyUpdateUserSerializer(user, data=request.data)
+
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         except LazyUser.DoesNotExist:
             return Response({'detail': 'User not found.'}, status=status.HTTP_404_NOT_FOUND)
-
-        serializer = LazyUpdateUserSerializer(user, data=request.data)
-
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
+
+
+class LazyDeleteUserAPIView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    # Deletes a user with the specified ID
+    def delete(self, request, user_id):
+        try:
+            user = LazyUser.objects.get(user_id=user_id)
+
+            # Check authentication and permissions
+            if not request.user.is_staff and request.user != user:
+                return Response({'detail': 'You are not authorized to delete this user.'}, status=status.HTTP_403_FORBIDDEN)
+
+            user.delete()
+            return Response({'detail': 'User deleted successfully'}, status=status.HTTP_204_NO_CONTENT)
+        except LazyUser.DoesNotExist:
+            return Response({'detail': 'User not found.'}, status=status.HTTP_404_NOT_FOUND)
+        
+
 
 class UserListView(ListAPIView):
     queryset = LazyUser.objects.all()
