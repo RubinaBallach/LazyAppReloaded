5f4a07d10cea0ff00820f3ad967f0aadf48fd923
Letham01 <kusanagi29@ymail.com>
2024-02-29 13:48:29 +0100

    included functional faker test

--- lazyreload/apps/users/tests.py
+++ lazyreload/apps/users/tests.py
@@ -2,7 +2,8 @@ from rest_framework.test import APIClient, APITestCase
 from rest_framework import status
 from django.urls import reverse
 from django.contrib.auth import get_user_model
-from rest_framework.authtoken.models import Token
+from rest_framework.authtoken.models import Token 
+#from faker import Faker
 
 
 
